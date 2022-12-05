724556b09b5939836189f3d045ffb1f67a612875
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2022-12-05 10:26:19 +0200

    6.1.2. Creating an object from a class

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,13 @@
 // Dart Apprentice by Eng. Fady Fawzy
 // Chapter 6: Classes
 
-void main() {}
+import 'user.dart';
+
+void main() {
+  dartClasses();
+}
+
+void dartClasses() {
+  final user = User();
+  print(user);
+}
